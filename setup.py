b4e0dba22496f64ba698e4b593a2c4f31166832d
Jianing Lu <jl7003@nyu.edu>
2018-12-03 15:47:59 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a molecular energy and ligand stability prediction model bl based on deep neural tensor networks and MMFF optimized geometries',
+    author='Jianing Lu',
+    license='MIT',
+)
